9201f6554e421ff9c7d5cf4c0d19d318e3061007
Pedro Rafante <pedrorafante@gmail.com>
2023-04-26 15:03:12 -0300

    das

--- lucas/config.php
+++ lucas/config.php
@@ -2,6 +2,6 @@
 define('HOST', 'localhost');
 define('USER', 'u751391022_lucas');
 define('PASS', 'Admin123456');
-define('BASE', 'u751391022_uarles');
+define('BASE', 'u751391022_lucas');
 
 $conn = new mysqli(HOST, USER, PASS, BASE);
