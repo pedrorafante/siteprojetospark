6021e5fc68935762014ef3ab74772afb048345de
Pedro Rafante <pedrorafante@gmail.com>
2023-04-26 15:02:21 -0300

    dsadas

--- lucas/salvar.php
+++ lucas/salvar.php
@@ -1,11 +1,13 @@
 <?php
 
 include 'config.php';
+echo "<script>alert('1');</script>";
 $email = $_POST["email"];
-
+echo "<script>alert('2');</script>";
 $sql = "INSERT INTO newlaster (email) VALUES('{$email}')";
+echo "<script>alert('3');</script>";
 $query = mysqli_query($conn, $sql);
-
+echo "<script>alert('4');</script>";
 if ($query) {
     echo "<script>alert('Email cadastrado com sucesso!'); window.location='index.php';</script>";
 } else {
